d2287b8bd2c79d15a1a41654b7230518aa1474ce
Neword <newordn@gmail.com>
2018-09-14 12:54:56 +0100

    Adding the main component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SettingSideBar from './components/SideBar/SettingSideBar';
+import Header from './components/Header/Header';
+import  NewPasswordForm from './components/NewPasswordForm/NewPasswordForm';
+
+class App extends Component {
+  render() {
+    return (
+             <NewPasswordForm/>
+    );
+  }
+}
+
+export default App;
