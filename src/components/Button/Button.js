b0f7941c4ad3168ce10b71f1e0823eadff557f9a
Neword <newordn@gmail.com>
2018-09-14 12:48:32 +0100

    Adding Button Component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react';
+import './Button.css';
+import PropTypes from 'prop-types';
+class Button extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {clickedHighlight:false};
+        this.handleClick = this.handleClick.bind(this);  
+        this.handleBlur = this.handleBlur.bind(this);  
+    }
+    handleClick()
+    {
+        this.setState({clickedHighlight:true});
+    }
+    handleBlur()
+    {
+        this.setState({ clickedHighlight: false });
+    }
+    render()
+    {
+        let higlight = "";
+        if(this.state.clickedHighlight)
+        higlight="aqua";
+        else
+        higlight ="";
+        let classValues = higlight + " "+ this.props.textStyle+ " " +  this.props.backgroundColor + " " +  this.props.alignment + " " + this.props.width+ " "+ this.props.height;
+        return(
+            <button className={classValues} onBlur={this.handleBlur} onClick={this.handleClick}>{this.props.text}</button>
+        );
+    }
+    static propTypes=
+    {
+        textStyle: PropTypes.string,
+        
+    }
+}
+
+export default Button;
