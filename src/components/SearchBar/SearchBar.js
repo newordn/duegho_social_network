c094e17fb8d88752c17935ec10f768552410731a
Neword <newordn@gmail.com>
2018-09-14 12:51:47 +0100

    Adding SearchBar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Input from '../Input/Input';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    
+    render()
+    {
+        return(
+           <Input id="searchBar" width="search_bar_width" type="text" placeholder="Rechercher sur Duegho" name="searchBar" />
+        )
+    }
+}
+
+export default SearchBar;
