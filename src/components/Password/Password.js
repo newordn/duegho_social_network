93350f5bf8a6b83c7ce45f5d79eab601f4f886bb
Neword <newordn@gmail.com>
2018-09-14 12:52:59 +0100

    Adding password Component

---
+++ src/components/Password/Password.js
@@ -0,0 +1,53 @@
+import React,{Component} from 'react';
+import Input from '../Input/Input';
+import PropTypes from 'prop-types';
+// the password input
+class Password extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state={};
+		this.onChange= this.onChange.bind(this);
+		this.regEx = new RegExp("^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]))(?=.{8,})");
+		
+	}
+	// checking if the password is well wrote
+	onChange(value)
+	{
+		if(value!=null)
+		{
+			if(!this.regEx.test(value))
+			{
+				this.setState({errorMessage:"Doit contenir au minimum 8 caractères dont au moins une minisucle, une majuscule et un chiffre.",
+					errorBorderColor:"input__border_error-color",showError:"inline"});
+			}
+			else
+			{
+				this.setState({errorMessage:"",
+					errorBorderColor:"input__border_success-color",showError:"none"});
+			}
+		}		
+	}
+
+	 render()
+	 {
+		return(
+			<Input id={this.props.password} name="password" width={this.props.width}
+		type="text" placeholder={this.props.placeholder} style="form__input" 
+		onChange={this.onChange} errorBorderColor={this.state.errorBorderColor}
+		errorMessage={this.state.errorMessage} showError={this.state.showError}/>
+	);
+	 }
+	static propTypes=
+	{
+		password : PropTypes.string,
+	}
+	static defaultProps=
+	{
+		id:"",
+			placeholder: "Mot de passe"
+	}
+}
+
+export default Password;
